19fd6ddb89fe9f8327e7caa78eacc0fa51d68288
Andre <andre.augusto22@hotmail.com>
2023-10-03 10:30:53 -0300

    Verify win AB#6

--- src/GameTrilha.GameDomain/Entities/Board.cs
+++ src/GameTrilha.GameDomain/Entities/Board.cs
@@ -440,7 +440,7 @@ public class Board
             _ => false
         };
 
-        if (!winner && ColorPiecesAmount[opponentColor] == 0)
+        if (!winner && PendingPieces[opponentColor] == 0)
             winner = !HaveValidMoves(opponentColor);
 
         return winner;
