52c0ab79cab79906187edae3c33481835a913238
Andre <andre.augusto22@hotmail.com>
2023-11-21 17:33:48 -0300

    Ranking

---
+++ src/GameTrilha.Domain/DTOs/Users/RankingProfile.cs
@@ -0,0 +1,21 @@
+﻿using GameTrilha.Domain.ValueObjects;
+
+namespace GameTrilha.Domain.DTOs.Users;
+
+public class RankingProfile
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public UserAvatar Avatar { get; set; }
+    public int Score { get; set; }
+    public int Position { get; set; }
+
+    public RankingProfile(Guid id, string name, UserAvatar avatar, int score, int position)
+    {
+        Id = id;
+        Name = name;
+        Avatar = avatar;
+        Score = score;
+        Position = position;
+    }
+}
